ee975d03acd7ed690a4e18a0267c6956810af3c7
wangdehai <wangdehai@yzw.cn>
2022-08-04 17:30:54 +0800

    feat: 定义多线程工具类

---
+++ src/main/java/com/github/dreamroute/common/util/AsyncUtil.java
@@ -0,0 +1,31 @@
+package com.github.dreamroute.common.util;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+
+/**
+ * 描述：异步处理工具类
+ *
+ * @author w.dehai
+ */
+public class AsyncUtil {
+
+    private AsyncUtil() {}
+
+    public  static <U> CompletableFuture<U> supply(Supplier<U> supplier) {
+        return CompletableFuture.supplyAsync(supplier);
+    }
+
+    public  static <U> CompletableFuture<U> supply(Supplier<U> supplier, Executor executor) {
+        return CompletableFuture.supplyAsync(supplier, executor);
+    }
+
+    public static CompletableFuture<Void> run(Runnable runnable) {
+        return CompletableFuture.runAsync(runnable);
+    }
+
+    public static CompletableFuture<Void> run(Runnable runnable, Executor executor) {
+        return CompletableFuture.runAsync(runnable, executor);
+    }
+}
